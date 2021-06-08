ef982dd270563c9667d8c74c1ca9efea0bb43143
Agkesos <agkesos@omniworks.gr>
2021-06-09 00:10:58 +0300

    Add cors for localhost:4200

--- BOTestData/BOTestData/Startup.cs
+++ BOTestData/BOTestData/Startup.cs
@@ -20,6 +20,7 @@ namespace BOTestData
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
 
             services.AddControllers();
 
@@ -42,6 +43,9 @@ namespace BOTestData
             }
 
             app.UseRouting();
+
+            app.UseCors(builder =>
+                builder.WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
